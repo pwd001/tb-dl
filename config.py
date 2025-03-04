09664aac26d89341f717c502c8a1226465fa30f1
pwd001 <physicswallahkiduniya@gmail.com>
2025-03-05 00:10:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 23728809 # Your Telegram API ID
+API_HASH = "5d7165501181e2ef8000f333fe529678"
+BOT_TOKEN = "7850176671:AAHTJ-nK-Vv3WClhipJnlP3bxWaihAONfS0"
+FORCE_SUB = "SB_BKUP" # Channel Username without @
+API_URL = "https://tboxdownloader.ytansh038.workers.dev/?url="
